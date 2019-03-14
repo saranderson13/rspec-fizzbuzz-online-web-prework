46e3ce707d760f3f136084e32be78448974520c9
Sarah Anderson <saranderson13@gmail.com>
2019-03-14 18:05:30 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,15 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+def fizzbuzz(num)
+  test_3 = num % 3
+  test_5 = num % 5
+  
+  if test_3 == 0 && test_5 == 0
+    "FizzBuzz"
+  elsif test_3 == 0 && test_5 != 0
+    "Fizz"
+  elsif test_3 != 0 && test_5 == 0
+    "Buzz"
+  end
+end
