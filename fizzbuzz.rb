990459a4fa487040cb2fe3896ec215c0184265e8
Sarah Anderson <saranderson13@gmail.com>
2019-03-14 18:03:51 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,5 +2,14 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(num)
+  test_3 = num % 3
+  test_5 = num % 5
   
+  if test_3 == 0 && test_5 == 0
+    "FizzBuzz"
+  elsif test_3 == 0 && test_5 != 0
+    "Fizz"
+  elsif test_3 != 0 && test_5 == 0
+    "Buzz"
+  end
 end
